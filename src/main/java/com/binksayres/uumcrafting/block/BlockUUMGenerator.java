b89775c8957bf4614c93053945c5c6a0fac35e3b
Bink Sayres <binksayres@hotmail.com>
2014-10-15 23:46:37 -0400

    Added uumGenerator
    Added an empty block that will become the UUM Generator.

---
+++ src/main/java/com/binksayres/uumcrafting/block/BlockUUMGenerator.java
@@ -0,0 +1,12 @@
+package com.binksayres.uumcrafting.block;
+
+public class BlockUUMGenerator extends BlockMaster 
+{
+	public BlockUUMGenerator() 
+	{
+		super();
+		this.setBlockName("uumGenerator");
+		this.setBlockTextureName("uumGenerator");
+	}
+
+}

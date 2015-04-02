6eeab7f7e7f6d88416a99e8ae2283795b59de63f
Bink Sayres <binktcc@hotmail.com>
2015-04-02 11:48:33 -0400

    Forge 10.13.2.1291 & IntelliJ IDEA
    Rebuilt repository for the lasted recommended version of Forge.
    Switched to IntelliJ IDEA instead of eclipse with a successful build and
    run.

--- src/main/java/com/binksayres/uumcrafting/references/Reference.java
+++ src/main/java/com/binksayres/uumcrafting/references/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
 	public static final String MOD_ID = "UUMCrafting";
 	public static final String MOD_NAME = "UU-Matter Crafting";
-	public static final String MOD_VERSION = "1.7.10-1.0";
+	public static final String MOD_VERSION = "1.7.10-1.2.1";
 	public static final String CLIENT_PROXY_CLASS = "com.binksayres.uumcrafting.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "com.binksayres.uumcrafting.proxy.ServerProxy";
 	public static final String GUI_FACTORY_CLASS = "com.binksayres.uumcrafting.client.gui.GuiFactory";
